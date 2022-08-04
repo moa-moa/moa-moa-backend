9fd318a190bb1c53dc66b6d8b3c1e7845fa9903e
yl9517 <yl9517@naver.com>
2022-08-04 13:50:21 +0900

    apply swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './common/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  setupSwagger(app);
 
   await app.listen(3000);
 }
 bootstrap();
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Moa-Moa API')
+    .setDescription('description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+}
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ description: '이메일' })
   email: string;
+  @ApiProperty({ description: '이름' })
   name: string;
 }

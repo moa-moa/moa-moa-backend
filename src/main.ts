0d6de85cdeab1a09e62867d1eada32d703e48d97
yl9517 <yl9517@naver.com>
2022-08-04 13:53:03 +0900

    type validator 추가

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -10,6 +10,14 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
 
   setupSwagger(app);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
   @ApiProperty({ description: '이메일' })
   email: string;
+
+  @IsString()
   @ApiProperty({ description: '이름' })
   name: string;
 }

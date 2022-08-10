50f85c2514d660af600f1c5f28b1e4434537d63a
yl9517 <yl9517@naver.com>
2022-08-10 10:30:05 +0900

    pass the club service test

--- src/club/club.service.spec.ts
+++ src/club/club.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { PrismaModule } from '../common/prisma.module';
 import { ClubService } from './club.service';
 
 describe('ClubService', () => {
   let service: ClubService;
 
+  const prismaClient = new PrismaClient();
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule.forTest(prismaClient)],
       providers: [ClubService],
     }).compile();
 

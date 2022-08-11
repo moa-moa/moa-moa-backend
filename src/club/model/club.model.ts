d2839e87f578ce0cd46be7a3675b0b28fced6eca
yl9517 <yl9517@naver.com>
2022-08-11 12:54:52 +0900

    apply club swagger

--- src/club/model/club.model.ts
+++ src/club/model/club.model.ts
@@ -1,8 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Club {
+  @ApiProperty({ description: 'id' })
   readonly id: number;
+  @ApiProperty({ description: '카테고리 id' })
   categoryId: number;
+  @ApiProperty({ description: '타이틀' })
   title: string;
+  @ApiProperty({ description: '내용' })
   description: string;
+  @ApiProperty({ description: 'user id' })
   owner: string;
+  @ApiProperty({ description: '최대인원' })
   max?: number;
 }

a7c996949a57e517481a73530f44d50cc13162cc
yl9517 <yl9517@naver.com>
2022-08-17 15:46:38 +0900

    add `image` module - connect users avatar

--- src/image/dto/update-avatar.dto.ts
+++ src/image/dto/update-avatar.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-export class UpdateUserDto {
+export class UpdateAvatarDto {
   @IsString()
   @ApiProperty({ description: '프로필사진' })
   avatar: string;

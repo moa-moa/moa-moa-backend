5c279b0a6e0945e8e8466a8b437486afe3ea9347
yl9517 <yl9517@naver.com>
2022-08-18 17:20:07 +0900

    add cloud storage-이미지를 업로드하면 gcloud bucket에 저장됩니다.

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CloudStorageService } from '../common/cloud-storage.service';
 import { ImageService } from './image.service';
 
 @Module({
-  providers: [ImageService],
+  providers: [ImageService, CloudStorageService],
   exports: [ImageService],
 })
 export class ImageModule {}

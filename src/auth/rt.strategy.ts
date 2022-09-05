73d733187bb51cd2b6c167a84e397002b291d29e
summer <80736033+yl9517@users.noreply.github.com>
2022-09-05 12:42:01 +0900

    유효기간이 만료된 refresh token이면 로그아웃합니다

--- src/auth/rt.strategy.ts
+++ src/auth/rt.strategy.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { JwtPayload } from './jwtPayload.type';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-  constructor() {
+  constructor(
+    private jwtService: JwtService,
+    private userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (req: Request) => {
@@ -15,15 +20,22 @@ export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
       ]),
       secretOrKey: process.env.JWT_SECRET,
       passReqToCallback: true,
+      // //true로 설정하면 Passport에 토큰 검증을 위임하지 않고 직접 검증, false는 Passport에 검증 위임
+      ignoreExpiration: true,
     });
   }
 
   validate(req: Request, payload: JwtPayload) {
     const refreshToken = req.cookies['refreshToken'];
-
-    return {
-      ...payload,
-      refreshToken,
-    };
+    try {
+      this.jwtService.verify(refreshToken, { secret: process.env.JWT_SECRET });
+      return {
+        ...payload,
+        refreshToken,
+      };
+    } catch (e: any) {
+      this.userService.logout(payload.sub);
+      throw new HttpException('토큰이 만료되었습니다.', 410);
+    }
   }
 }

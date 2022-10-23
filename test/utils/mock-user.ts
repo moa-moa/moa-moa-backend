c6cb0689087dcb14d7dbce5ac4b0a900970a25f7
yl9517 <yl9517@naver.com>
2022-10-23 19:07:52 +0900

    test mockData 추가

--- test/utils/mock-user.ts
+++ test/utils/mock-user.ts
@@ -11,6 +11,17 @@ export const validMockUser = (
   hashedRt: '123',
 });
 
+export const validMockUser2 = (
+  fields?: Partial<Prisma.UserCreateInput>,
+): CreateUserDto => ({
+  id: '4321',
+  email: 'test2@moa.com',
+  provider: 'google',
+  name: 'test2name',
+  ...fields,
+  hashedRt: '321',
+});
+
 export const invalidMockUser = (
   fields?: Partial<Prisma.UserCreateInput>,
 ): CreateUserDto => ({
